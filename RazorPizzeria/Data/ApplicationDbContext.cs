ec3fef105730fb1cee8d2d52a3604cfa7098e604
Ahmet Ulucay <57094137+ahmetulucay@users.noreply.github.com>
2024-04-30 14:28:55 +0200

    Update ApplicationDbContext.cs

--- RazorPizzeria/Data/ApplicationDbContext.cs
+++ RazorPizzeria/Data/ApplicationDbContext.cs
@@ -2,13 +2,8 @@
 using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Data;
-
 public class ApplicationDbContext : DbContext
 {
     public DbSet<PizzaOrder> PizzaOrders { get; set; }
-
-	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-	{
-
-	}
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
 }
